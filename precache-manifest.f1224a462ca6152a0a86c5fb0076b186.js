8451c7e4aa5bab0a5ca44a984d8677fdb9f88d48
xtaaaal <crystal.hon@arch-partnership.com>
2019-12-27 11:00:10 +0800

    Updates

---
+++ precache-manifest.f1224a462ca6152a0a86c5fb0076b186.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0486c6b1ea3d101432cbdf5ee4c9b3cc",
+    "url": "/fukuoka/index.html"
+  },
+  {
+    "revision": "b1bd2c4c3aafe9c70efa",
+    "url": "/fukuoka/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "fce396d0157006206c19",
+    "url": "/fukuoka/static/js/2.1c937f35.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/fukuoka/static/js/2.1c937f35.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b1bd2c4c3aafe9c70efa",
+    "url": "/fukuoka/static/js/main.175f4db1.chunk.js"
+  },
+  {
+    "revision": "c75b44672f3d643e6cd2",
+    "url": "/fukuoka/static/js/runtime-main.a943e0f6.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/fukuoka/static/media/logo.5d5d9eef.svg"
+  }
+]);
